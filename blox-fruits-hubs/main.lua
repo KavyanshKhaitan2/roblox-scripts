0ce39b2f746be24a7f9dde77fb7aebdedf1860e6
Kavyansh Khaitan <73186427+KavyanshKhaitan2@users.noreply.github.com>
2025-01-14 09:07:58 +0530

    Update main.lua

--- blox-fruits-hubs/main.lua
+++ blox-fruits-hubs/main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://github.com/KavyanshKhaitan2/roblox-scripts/raw/refs/heads/main/blox-fruits-hubs/hub.lua'))
+loadstring(game:HttpGet('https://github.com/KavyanshKhaitan2/roblox-scripts/raw/refs/heads/main/blox-fruits-hubs/hub.lua'))()
