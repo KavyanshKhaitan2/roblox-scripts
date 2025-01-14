53fa65c5da303a8d123ad165332d23de8e17b704
Kavyansh Khaitan <73186427+KavyanshKhaitan2@users.noreply.github.com>
2025-01-14 08:33:57 +0530

    Create hub.lua

---
+++ blox-fruits-hubs/hub.lua
@@ -0,0 +1,147 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local MaxSpeed = 300 -- Studs per second 380 no flag but kick
+
+local LocalPlayer = game:GetService("Players").LocalPlayer
+local Locations = workspace._WorldOrigin.Locations
+
+local function getCharacter()
+    if not LocalPlayer.Character then
+        LocalPlayer.CharacterAdded:Wait()
+    end
+    LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+    return LocalPlayer.Character
+end
+
+local function DistanceFromPlrSort(ObjectList: table)
+    local RootPart = getCharacter().LowerTorso
+    table.sort(ObjectList, function(ChestA, ChestB)
+        local RootPos = RootPart.Position
+        local DistanceA = (RootPos - ChestA.Position).Magnitude
+        local DistanceB = (RootPos - ChestB.Position).Magnitude
+        return DistanceA < DistanceB
+    end)
+end
+
+local UncheckedChests = {}
+local FirstRun = true
+
+local function getChestsSorted()
+    if FirstRun then
+        FirstRun = false
+        local Objects = game:GetDescendants()
+        for i, Object in pairs(Objects) do
+            if Object.Name:find("Chest") and Object.ClassName == "Part" then
+                table.insert(UncheckedChests, Object)
+            end
+        end
+    end
+    local Chests = {}
+    for i, Chest in pairs(UncheckedChests) do
+        if Chest:FindFirstChild("TouchInterest") then
+            table.insert(Chests, Chest)
+        end
+    end
+    DistanceFromPlrSort(Chests)
+    return Chests
+end
+
+local function toggleNoclip(Toggle: boolean)
+    for i,v in pairs(getCharacter():GetChildren()) do
+        if v.ClassName == "Part" then
+            v.CanCollide = not Toggle
+        end
+    end
+end
+
+local function Teleport(Goal: CFrame, Speed)
+    if not Speed then
+        Speed = MaxSpeed
+    end
+    toggleNoclip(true)
+    local RootPart = getCharacter().HumanoidRootPart
+    local Magnitude = (RootPart.Position - Goal.Position).Magnitude
+
+    RootPart.CFrame = RootPart.CFrame
+    
+    while not (Magnitude < 1) do
+        local Direction = (Goal.Position - RootPart.Position).unit
+        RootPart.CFrame = RootPart.CFrame + Direction * (Speed * wait())
+        Magnitude = (RootPart.Position - Goal.Position).Magnitude
+    end
+    toggleNoclip(false)
+end
+
+
+wait = task.wait
+
+local Window = Rayfield:CreateWindow({
+   Name = "ChestHop for Blox Fruits by '@kavyansh.' on discord",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "Chest hop for Blox Fruits",
+   LoadingSubtitle = "by '@kavyansh.' on discord",
+   Theme = "Default",
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "BFChestHopKavyansh"
+   },
+
+   Discord = {
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "CashGen",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+
+local MainTab = Window:CreateTab("Home", "home")
+local ChestHopSection = MainTab:CreateSection("Chest Hop")
+
+Rayfield:Notify({
+   Title = "Chesthop script loaded",
+   Content = "Created by 'kavyansh.' on discord\nUse [K] to open/close the GUI.",
+   Duration = 6.5,
+   Image = "rabbit",
+})
+
+local ChestHopToggle = MainTab:CreateToggle({
+   Name = "Enable Chest Hop",
+   CurrentValue = false,
+   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+        print("toggle")
+    end
+})
+
+ChestHopToggle:Set(false)
+
+local StartChestHop = MainTab:CreateButton({
+   Name = "Start Chesthop",
+   Callback = function()
+        wait(1)
+        while ChestHopToggle.CurrentValue do
+            wait()
+            local Chests = getChestsSorted()
+            if #Chests > 0 then
+                Teleport(Chests[1].CFrame)
+            else
+                -- You can put serverhop here
+            end
+        end
+    end,
+})
